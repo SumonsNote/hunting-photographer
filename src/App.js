a1cfcf5fc00143ecc79332f7b5d25448408c0e8f
Sumonsnote <sohankhance@gmail.com>
2022-04-17 00:01:27 +0600

    header done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home';
+import ServicesSection from './Pages/ServicesSection';
+import Blogs from './Pages/Blogs';
+import About from './Pages/About';
+import Navbar from './ShareFiles/Navbar';
+import Login from './Authentication/Login';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/serviceSection' element={<ServicesSection></ServicesSection>}></Route>
+        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+      </Routes>
     </div>
   );
 }
--- src/ShareFiles/Navbar.js
+++ src/ShareFiles/Navbar.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <div>
-            <h2>Navbar Page</h2>
-        </div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light container">
+            <div className="container-fluid">
+                <Link className="navbar-brand text fs-3 text-uppercase " to='/'>Hunting Photographer</Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse d-flex justify-content-end" id="navbarNavAltMarkup">
+                    <div className="navbar-nav">
+                        <Link className="nav-link active" aria-current="page" to='/home'>Home</Link>
+                        <Link className="nav-link ms-3" to='/serviceSection'>Section Services</Link>
+                        <Link className="nav-link ms-3" to='/blogs'>Blogs</Link>
+                        <Link className="nav-link ms-3" to='/about'>About</Link>
+                        <Link className="nav-link ms-3" to='/login'>Login</Link>
+                    </div>
+                </div>
+            </div>
+        </nav>
     );
 };
 
