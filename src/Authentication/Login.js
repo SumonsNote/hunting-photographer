849692f908929004c3152fb9685b7ccb9ee8d830
Sumonsnote <sohankhance@gmail.com>
2022-04-17 03:07:58 +0600

    register and login

--- src/Authentication/Login.js
+++ src/Authentication/Login.js
@@ -1,18 +1,54 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import SocialLogin from './SocialLogin';
+import auth from './../Firebase/Firebase.init';
 
 const Login = () => {
+    const navigate = useNavigate()
+    const location = useLocation()
+    
+    let from = location.state?.from?.pathname || "/";
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useSignInWithEmailAndPassword(auth);
+
+      const handleSignIn = (e) => {
+        e.preventDefault()
+       const email = e.target.email.value;
+       const pass = e.target.password.value;
+       signInWithEmailAndPassword(email, pass)
+      }
+      let errorElement;
+
+      if (error) {
+          errorElement =
+              <div>
+                  <p className='text-danger'>Error: {error?.message}</p>
+              </div>
+  
+      }
+  
+      if (loading) {
+          return <p>Loading...</p>
+      }
+  
+      if (user) {
+          navigate(from, { replace: true });
+      }
     return (
         <div className='container w-25'>
         <h2 className='py-3'>Login</h2>
-            <form>
+            <form onSubmit={handleSignIn}>
                 <div className="form-outline mb-4">
-                    <input type="email" id="form2Example1" className="form-control" placeholder='Your Email' />
+                    <input type="email" name='email' id="form2Example1" className="form-control" placeholder='Your Email' />
                 </div>
 
                 <div className="form-outline mb-4">
-                    <input type="password" id="form2Example2" className="form-control" placeholder='Your Password' />
+                    <input type="password" name='password' id="form2Example2" className="form-control" placeholder='Your Password' />
                 </div>
 
                 <div className="row mb-4 d-flex align-items-center">
@@ -27,8 +63,8 @@ const Login = () => {
                         <button className='btn btn-link text-decoration-none text-success'>Forgot password?</button>
                     </div>
                 </div>
-
-                <button type="button" className="btn btn-success btn-block mb-4">Login</button>
+                {errorElement}
+                <button type="submit" className="btn btn-success btn-block mb-4">Login</button>
 
                 <div className="text-center">
                     <p>Not a member? <Link to='/register' className='text-success text-decoration-none'>Register</Link></p>
--- src/Authentication/Register.js
+++ src/Authentication/Register.js
@@ -1,10 +1,47 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import SocialLogin from './SocialLogin';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from './../Firebase/Firebase.init';
 
 const Register = () => {
+    const [agree, setAgree] = useState(false)
+    const navigate = useNavigate()
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useCreateUserWithEmailAndPassword(auth);
+
+    const handleRegister = (event) => {
+        event.preventDefault()
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+        createUserWithEmailAndPassword(email, password)
+        // console.log(name, email, password);
+    }
+
+    if (user) {
+        navigate('/home')
+    }
+    if (loading) {
+        return (
+            <div>
+                <p>Loading...</p>
+            </div>
+        );
+    }
+    if (error) {
+        return (
+            <div>
+                <p>Error: {error}</p>
+            </div>
+        );
+    }
     return (
-        <div>
+        <form onSubmit={handleRegister}>
             <section className="border-0">
                 <div className=" d-flex align-items-center ">
                     <div className="container">
@@ -14,46 +51,46 @@ const Register = () => {
                                     <div className="card-body p-5">
                                         <h2 className=" text-center mb-5">Create an account</h2>
 
-                                        <form>
+                                        <div>
 
                                             <div className=" mb-4">
-                                                <input type="text" id="form3Example1cg" className="form-control form-control-lg" name='name' placeholder='Your Name' />
+                                                <input type="text" id="form3Example1cg" className="form-control form-control-lg" name='name' placeholder='Your Name' required />
                                             </div>
 
                                             <div className="form-outline mb-4">
-                                                <input type="email" id="form3Example3cg" className="form-control form-control-lg" name='email' placeholder='Your Email' />
-
-                                            </div>
-
-                                            <div className="form-outline mb-4">
-                                                <input type="password" id="form3Example4cg" className="form-control form-control-lg" name='password' placeholder='Your Password' />
+                                                <input type="email" id="form3Example3cg" className="form-control form-control-lg" name='email' placeholder='Your Email' required />
 
                                             </div>
 
                                             <div className="form-outline mb-4">
-                                                <input type="password" id="form3Example4cdg" className="form-control form-control-lg" name='password' placeholder='Confirm Password' />
+                                                <input type="password" id="form3Example4cg" className="form-control form-control-lg" name='password' placeholder='Your Password' required />
+
+                                            </div>
+
+                                            <div className="form-outline mb-4">
+                                                <input type="password" id="form3Example4cdg" className="form-control form-control-lg" name='confirm-password' placeholder='Confirm Password' required />
 
                                             </div>
 
                                             <div className="form-check d-flex justify-content-center mb-5">
-                                                <input
+                                                <input onClick={() => setAgree(!agree)}
                                                     className="form-check-input me-2"
                                                     type="checkbox"
                                                     value=""
                                                     id="form2Example3cg"
                                                 />
-                                                <label className="form-check-label" for="form2Example3g">
+                                                <label className="form-check-label" htmlFor="form2Example3g">
                                                     I agree all statements in Terms of service
                                                 </label>
                                             </div>
 
                                             <div className="d-flex justify-content-center">
-                                                <button type="button" className="btn btn-success btn-block btn-lg text-white">Register</button>
+                                                <button disabled={!agree} type="submit" className="btn btn-success btn-block btn-lg text-white">Register</button>
                                             </div>
 
                                             <p className="text-center text-muted mt-5 mb-0">Have already an account? <Link to='/login' className="fw-bold text-success text-decoration-none">Login here</Link></p>
 
-                                        </form>
+                                        </div>
                                         <SocialLogin></SocialLogin>
                                     </div>
                                 </div>
@@ -62,7 +99,7 @@ const Register = () => {
                     </div>
                 </div>
             </section>
-        </div>
+        </form>
     );
 };
 
