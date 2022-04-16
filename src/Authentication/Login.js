4b1dbae5eccdb53dab8b9714fcf3673b5ff6bc6e
Sumonsnote <sohankhance@gmail.com>
2022-04-17 01:54:39 +0600

    login with google

--- src/Authentication/Login.js
+++ src/Authentication/Login.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import SocialLogin from './SocialLogin';
 
 const Login = () => {
     return (
-        <div>
-            <h2>Login</h2>
+        <div className='container w-25'>
+            <form>
+                <div className="form-outline mb-4">
+                    <input type="email" id="form2Example1" className="form-control" placeholder='Your Email' />
+                </div>
+
+                <div className="form-outline mb-4">
+                    <input type="password" id="form2Example2" className="form-control" placeholder='Your Password' />
+                </div>
+
+                <div className="row mb-4 d-flex align-items-center">
+                    <div className="col d-flex justify-content-center">
+                        <div className="form-check">
+                            <input className="form-check-input" type="checkbox" value="" id="form2Example31" />
+                            <label className="form-check-label" htmlFor="form2Example31"> Remember me </label>
+                        </div>
+                    </div>
+
+                    <div className="col">
+                        <button className='btn btn-link text-decoration-none'>Forgot password?</button>
+                    </div>
+                </div>
+
+                <button type="button" className="btn btn-success btn-block mb-4">Login</button>
+
+                <div className="text-center">
+                    <p>Not a member? <a href="#!">Register</a></p>
+                </div>
+            </form>
+            <SocialLogin></SocialLogin>
         </div>
     );
 };
---
+++ src/Authentication/SocialLogin.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { useSignInWithFacebook, useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from './../Firebase/Firebase.init';
+import facebook from '../Images/facebook.png'
+import google from '../Images/google.png'
+import { useNavigate } from 'react-router-dom';
+
+const SocialLogin = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const [signInWithFacebook, user1, loading1, error1] = useSignInWithFacebook(auth);
+    const navigate = useNavigate()
+
+    let errorElement;
+
+    if (error || error1) {
+        errorElement =
+            <div>
+                <p className='text-danger'>Error: {error?.message} {error1.message}</p>
+            </div>
+    }
+
+    if (user || user1) {
+        navigate('/home')
+    }
+    if (loading || loading1) {
+        <p>Loading...</p>
+    }
+    return (
+        <div>
+            <div className='d-flex align-items-center py-2'>
+                <div style={{ height: '1px' }} className='w-50 bg-success'></div>
+                <div className='px-2'>or</div>
+                <div style={{ height: '1px' }} className='w-50 bg-success'></div>
+            </div>
+            {errorElement}
+            <div className=''>
+                <button onClick={() => signInWithGoogle()} className='btn btn-light my-3 w-75'>
+                    <img className='mx-2' style={{ width: '30px' }} src={google} alt="" />
+                    <span>Continue With Google</span>
+                </button>
+                <button onClick={() => signInWithFacebook()} className='btn btn-light my-3 w-75'>
+                    <img className='mx-2' style={{ width: '30px' }} src={facebook} alt="" />
+                    <span>Continue With Facebook</span>
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default SocialLogin;
