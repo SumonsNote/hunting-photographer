aa67a9aa5a2881ea5c1ec6eb4eb4ff2d648b0d63
Sumonsnote <sohankhance@gmail.com>
2022-04-18 02:38:41 +0600

    customlink

--- src/ShareFiles/Navbar.js
+++ src/ShareFiles/Navbar.js
@@ -4,6 +4,7 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link, useNavigate } from 'react-router-dom';
 import auth from './../Firebase/Firebase.init';
 import Loading from './Loading';
+import CustomLink from './CustomLink';
 
 const Navbar = () => {
     const [user, loading, error] = useAuthState(auth);
@@ -23,20 +24,20 @@ const Navbar = () => {
     return (
             <nav className="navbar navbar-expand-lg navbar-light container sticky-top">
             <div className="container-fluid">
-                <Link className="navbar-brand text fs-3 text-uppercase " to='/'>Hunting Photographer</Link>
+                <CustomLink className="navbar-brand text fs-3 text-uppercase " to='/'>Hunting Photographer</CustomLink>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse d-flex justify-content-end" id="navbarNavAltMarkup">
                     <div className="navbar-nav">
-                        <Link className="nav-link active" aria-current="page" to='/home'>Home</Link>
-                    <Link className="nav-link ms-3" to='/services'>Services</Link>
-                        <Link className="nav-link ms-3" to='/blogs'>Blogs</Link>
-                        <Link className="nav-link ms-3" to='/about'>About</Link>
+                        <CustomLink className="nav-link active" aria-current="page" to='/home'>Home</CustomLink>
+                    <CustomLink className="nav-link ms-3" to='/services'>Services</CustomLink>
+                        <CustomLink className="nav-link ms-3" to='/blogs'>Blogs</CustomLink>
+                        <CustomLink className="nav-link ms-3" to='/about'>About</CustomLink>
                         {
                             user ? 
                             <button className='btn btn-link text-dark text-decoration-none' onClick={handleSignOut}>Sign out</button> :
-                            <Link className="nav-link ms-3" to='/login'>Login</Link>}
+                            <CustomLink className="nav-link ms-3" to='/login'>Login</CustomLink>}
                     </div>
                 </div>
             </div>
